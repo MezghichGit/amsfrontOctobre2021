66030380ae89f014ff633d39c68e73a19c4db87e
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-23 16:34:23 +0100

    crud sauf upadte

--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -9,23 +9,23 @@ export class ProviderService {
   provider: any;
   constructor(private Http: HttpClient) { }
   listProviders() {
-  return this.Http.get(this.urlProviders + '/list');
+    return this.Http.get(this.urlProviders + '/list');
   }
-  createProvider(myform:any) {
-  this.provider = {
-  'name': myform.value.providerName,
-  'email': myform.value.providerEmail,
-  'address': myform.value.providerAdress
+  createProvider(myform: any) {
+    this.provider = {
+      'name': myform.value.providerName,
+      'email': myform.value.providerEmail,
+      'address': myform.value.providerAdress
+    }
+    return this.Http.post(this.urlProviders + '/add', this.provider);
   }
-  return this.Http.post(this.urlProviders + '/add', this.provider);
+  updateProvider(myObj: any) {
+    return this.Http.put(this.urlProviders + '/' + myObj['id'], myObj);
   }
-  updateProvider(myObj:any) {
-  return this.Http.put(this.urlProviders + '/' + myObj['id'], myObj);
+  deleteProvider(myObj: any) {
+    return this.Http.delete(this.urlProviders + '/' + myObj['id'], myObj)
   }
-  deleteProvider(myObj:any) {
-  return this.Http.delete(this.urlProviders + '/' + myObj['id'], myObj)
-  }
-  getProvider(id:any) {
-  return this.Http.get(this.urlProviders + '/' + id)
+  getProvider(id: any) {
+    return this.Http.get(this.urlProviders + '/' + id)
   }
 }
