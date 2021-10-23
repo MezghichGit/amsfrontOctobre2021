71d8d68fcca4f654fe4637e583bdbdde031fdbe5
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-23 15:43:04 +0100

    List Provider OK

--- src/app/list-provider/list-provider.component.ts
+++ src/app/list-provider/list-provider.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProviderService } from './../services/provider.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-list-provider',
   templateUrl: './list-provider.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProviderComponent implements OnInit {
 
-  constructor() { }
+  providers: any;
+  constructor(private service: ProviderService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.service.listProviders().subscribe(
+      response => {
+        this.providers = response;
+      }
+    );
+  }
+
+  deleteProvider(myObj: any) {
+    //console.log(this.provider);
+    this.service.deleteProvider(myObj).subscribe(response => {
+      console.log(response);
+      this.refreshListProviders();
+    })
+  }
+  refreshListProviders() {
+    this.service.listProviders().subscribe(
+      response => {
+        this.providers = response;
+      }
+    );
+  }
+  updateProvider(myObj: any) {
+    this.router.navigate(['updateProvider' + '/' + myObj['id']]);
   }
 
 }
